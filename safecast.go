96562dd0f3b85ff15072c15cf321a03451dd1b62
CHEN Feng <chen3feng@gmail.com>
2022-08-15 00:00:16 +0800

    add missing files

---
+++ safecast.go
@@ -0,0 +1,8 @@
+// Package safecast provide a safe way to cast a numeric value from type A to type B,
+// with overflow and underflow check.
+package safecast
+
+// All the code are generated.
+
+//go:generate sh -c "python3 generate.py | gofmt > casts.go"
+//go:generate sh -c "python3 generate_generic.py | gofmt > generics.go"
---
+++ safecast.py
@@ -0,0 +1,6 @@
+
+ALL_INT_BITS = ('', '8', '16', '32', '64')
+
+
+def to_camel_case(type):
+    return type[0].upper() + type[1:]
